363f0a4a5e9f166bdf02bd2af13be458e1d5883a
Brendan McCray <rbmccr@gmail.com>
2019-08-27 18:54:33 -0500

    Added package.json, revised file structure, and relocated code in watch() to execute callback before returning error value inside of catch block.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-class Detector {
+class Snatcher {
 
   constructor(x = null) {
     if (x) { this.config = x; }
@@ -6,9 +6,8 @@ class Detector {
 
   acceptedConsoleMethods = ['error', 'group', 'info', 'log', 'table', 'trace', 'warn'];
   config = null;
-  errorMsg = 'Detector watch() method contains an invalid config object. Check class instances and individual function calls.'
+  errorMsg = 'Snatcher watch() method contains an invalid config object. Check class instances and individual function calls.'
 
-  // wrapper method that encapsulates a function and provides a user-defined error response handle
   watch(callback, config = this.config) {
     if (!config) { return console.error(this.errorMsg); }
     const ce = config.error;
@@ -20,11 +19,11 @@ class Detector {
       if (ce) {
         if (ce.report && this.acceptedConsoleMethods.includes(ce.report)) { console[ce.report](err); }
         //---
-        if (ce.provideErr) { return err; }
         else if (ce.execute) {
           const x = ce.execute();
           if (x) { return x; }
         }
+        if (ce.provideErr) { return err; }
         else if (ce.default) { return ce.default; }
       }
     }
@@ -39,16 +38,3 @@ class Detector {
   }
 
 }
-
-const config = {
-  error: {
-    default: null, // return this value if callback doesn't return
-    execute: null,
-    provideErr: false, // set to true if you want to receive the err as the first argument in your callback.
-    report: null, // 'error', 'info', 'log', 'warn' -- basically any console method that you want to use to display your error
-  },
-  finally: {
-    default: null, // return this value if callback doesn't return
-    execute: null, // callback (if value is returned from callback, then same value is returned from finally block)
-  }
-};
